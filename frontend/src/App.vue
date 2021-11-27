5a313c83d92a0f94bf436bf5b511cd2f8359f3bc
Valentin Seehausen <valentin.seehausen@gmail.com>
2021-11-27 14:20:27 +0100

    navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -29,9 +29,9 @@
         >
           <v-list-item to="/">
             <v-list-item-icon>
-              <v-icon>mdi-view-dashboard</v-icon>
+              <v-icon>mdi-home</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>Dashboard</v-list-item-title>
           </v-list-item>
 
           <v-list-item to="/profile">
